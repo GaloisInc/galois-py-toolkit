08413dbd48fb601b45b398de73218b9de2b95985
Andrew Kent <andrew@galois.com>
2021-02-23 16:21:16 -0800

    test: enable eval server tests when CRYPTOL_SERVER is set (#15)

--- tests/cryptol_eval/test_basics.py
+++ tests/cryptol_eval/test_basics.py
@@ -16,16 +16,20 @@ class CryptolEvalServerTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
-        self.c = cryptol.connect("cryptol-eval-server socket --module M", cryptol_path=dir_path)
+        if command := os.getenv('CRYPTOL_SERVER'):
+            self.c = cryptol.connect(f'{command} socket --module M', cryptol_path=dir_path)
+        else:
+            raise ValueError('CRYPTOL_SERVER environment variable not set (eval server tests currently only work with a local executable)')
 
 
     def test_evaluation(self):
-        c = self.c
-        res = c.call('f', BV(size=8,value=0xff)).result()
-        self.assertEqual(res, [BV(size=8,value=0xff), BV(size=8,value=0xff)])
+        if self.c:
+            c = self.c
+            res = c.call('f', BV(size=8,value=0xff)).result()
+            self.assertEqual(res, [BV(size=8,value=0xff), BV(size=8,value=0xff)])
 
-    def test_disallowed_ops(self):
-        pass # TODO/FIXME
+    # def test_disallowed_ops(self):
+    #     pass # TODO/FIXME
 
 
 if __name__ == "__main__":
